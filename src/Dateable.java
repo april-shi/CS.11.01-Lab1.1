76e7990eb2f09fc3228fa4bb2896b892ebb9d74a
april <aprils2026@student.cis.edu.hk>
2023-09-01 19:56:24 +0800

    Lab 1 completed

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
 public class Dateable {
 
     public static void main(String[] args) {
-        
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter your age");
+        int age = Integer.parseInt(scan.nextLine());
+        int math1 = 7+ (age/2);
+        String output = age + "-year olds should date somebody who is at least " + math1 + " years old.";
+        System.out.println(output);
+
     }
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
 public class Pages {
 
     public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Please enter your age");
+        int age = Integer.parseInt(scan.nextLine());
+        int mathhaha = 100-age;
+
+        System.out.println(age + "-year olds should read at least " + mathhaha + " pages before giving up on a book.");
 
     }
 }
